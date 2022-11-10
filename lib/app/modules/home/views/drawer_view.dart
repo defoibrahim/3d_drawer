d34ab0c07f60836d8480dba187f6bd486ba1710a
Dafallah Ibrahim <dodokaka10@gmail.com>
2022-11-11 02:54:18 +0400

    performance improvment

--- lib/app/modules/home/views/drawer_view.dart
+++ lib/app/modules/home/views/drawer_view.dart
@@ -14,55 +14,70 @@ class DrawerView extends GetView<HomeController> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           mainAxisSize: MainAxisSize.max,
-          children: [
-            buildHeader(),
-            buildDrawerItem(),
+          children: const [
+            BuildHeader(),
+            BuildDrawerItem(),
           ],
         ),
       ),
     );
   }
+}
 
-  Widget buildHeader() => Column(
-        children: [
-          ClipOval(
-            child: Image.asset(
-              'assets/images/demon.jpg',
-              height: 120,
-              width: 120,
-            ),
-          ).paddingOnly(top: 16, left: 16, right: 16),
-          const SizedBox(height: 8),
-          const Text(
-            'Dafallah Ibrahim',
-            style: TextStyle(
-              fontSize: 22,
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-            ),
-          ).paddingOnly(top: 8, left: 16, right: 16, bottom: 16),
-        ],
-      );
+class BuildHeader extends StatelessWidget {
+  const BuildHeader({super.key});
 
-  Widget buildDrawerItem() => Column(
-        children: controller.all
-            .map(
-              (item) => ListTile(
-                onTap: () {
-                  controller.close();
-                },
-                title: Text(
-                  item.title,
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                  ),
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ClipOval(
+          child: Image.asset(
+            'assets/images/demon.jpg',
+            height: 120,
+            width: 120,
+          ),
+        ).paddingOnly(top: 16, left: 16, right: 16),
+        const SizedBox(height: 8),
+        const Text(
+          'Dafallah Ibrahim',
+          style: TextStyle(
+            fontSize: 22,
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ).paddingOnly(top: 8, left: 16, right: 16, bottom: 16),
+      ],
+    );
+  }
+}
+
+class BuildDrawerItem extends StatelessWidget {
+  const BuildDrawerItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = Get.find<HomeController>();
+    return Column(
+      children: controller.all
+          .map(
+            (item) => ListTile(
+              onTap: () {
+                controller.close();
+              },
+              title: Text(
+                item.title,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
                 ),
-                leading: Icon(item.icone, color: Colors.white),
-                contentPadding:
-                    const EdgeInsets.symmetric(horizontal: 24, vertical: 8),
               ),
-            )
-            .toList(),
-      );
+              leading: Icon(item.icone, color: Colors.white),
+              contentPadding:
+                  const EdgeInsets.symmetric(horizontal: 24, vertical: 8),
+            ),
+          )
+          .toList(),
+    );
+  }
 }
